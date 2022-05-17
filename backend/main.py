2314695007de3f958884950d4c063a576a16ab44
Nile Okomo <okomonile3@gmail.com>
2022-05-17 16:38:12 +0300

    Start endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from models import Student
+from pydantic import BaseModel, Json
+import models
+from db import SessionLocal
+
 
 app = FastAPI()
 
+class User(BaseModel):
+    fullname: str
+    email: str
+    number: str
+    role: str
+
+class Project(BaseModel):
+    projectName: str
+    projectDescription: str
+    studentID: int
+    evaluatorID: int = None
+    investorID: int = None
+    approved: bool
+    evaluatorComment: str = None
+    category: str
 
 origins = ['https://localhost:3000']
 
@@ -15,3 +33,39 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+db = SessionLocal()
+
+@app.post("/api/auth/register", status_code=201)
+def register(user: User):
+    new_user = models.User(
+        fullname=user.fullname,
+        email=user.email,
+        number=user.number,
+        role=user.role,
+    )
+    db.add(new_user)
+    db.commit()
+    return {"userId": new_user.userId}
+
+# Yet to be fixed
+@app.post("/api/project/upload/{userId}", status_code=201)
+def upload_project(userId: int, project: Project):
+    new_project = models.Project(
+        projectName=project.projectName,
+        projectDescription=project.projectDescription,
+        studentID={userId},
+        evaluatorID=project.evaluatorID,
+        investorID=project.investorID,
+        approved=project.approved,
+        evaluatorComment=project.evaluatorComment,
+        category=project.category,
+    )
+    db.add(new_project)
+    db.commit()
+    return {"projectId": new_project.projectId}
+
+@app.get("/api/project/{userId}", status_code=200)
+def get_project(userId: int):
+    my_project = db.query(models.Project).filter(models.Project.studentID==userId).all()
+    return my_project
