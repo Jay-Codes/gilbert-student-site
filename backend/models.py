d9db64cf35997d5f9432d5a6377679b2750139b0
Nile Okomo <okomonile3@gmail.com>
2022-05-17 16:38:23 +0300

    Include models for app

--- backend/models.py
+++ backend/models.py
@@ -1,22 +1,25 @@
-from pydantic import BaseModel
+from db import Base, engine
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text
 
 # Sample schema
-class User(BaseModel):
-    _id: int
-    username: str
-    password: str
-    email: str
-    name: str
-    is_student: bool
-    is_investor: bool
-    is_evaluator: bool
+class User(Base):
+    __tablename__ = 'users'
+    userId = Column(Integer, primary_key=True)
+    fullname = Column(String(60), nullable=False)
+    email = Column(String(30), nullable=False)
+    number = Column(String(20), nullable=False)
+    role = Column(String(20), nullable=False)
 
-class Project(BaseModel):
-    _id: int
-    name: str
-    description: str
-    status: str
-    owner: str
-    evaluators: list
-    investors: list
-    evaluations: list
+class Project(Base):
+    __tablename__ = 'projects'
+    projectId = Column(Integer, primary_key=True)
+    projectName = Column(String(60), nullable=False)
+    projectDescription = Column(Text, nullable=False)
+    studentID = Column(Integer, ForeignKey('users.userId'), nullable=True)
+    evaluatorID = Column(Integer, ForeignKey('users.userId'), nullable=True) 
+    investorID = Column(Integer, ForeignKey('users.userId'), nullable=True)
+    approved = Column(Boolean, nullable=False)
+    evaluatorComment = Column(Text, nullable=True)
+    category = Column(String(20), nullable=False)
+
+Base.metadata.create_all(bind=engine)
